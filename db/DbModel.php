5eb6765825601ac8049f79e817bff49d3bb6c8b4
Albi Fauzi <albifzi@gmail.com>
2020-10-04 12:23:25 +0700

    add delete function

--- db/DbModel.php
+++ db/DbModel.php
@@ -71,4 +71,19 @@ abstract class DbModel extends Model
         $statement->execute();
         return $statement->fetchAll();
     }
+
+    public function delete($where)
+    {
+        $tableName = static::tableName();
+        $attributes = array_keys($where);
+        $sql = implode("AND ", array_map(fn($attr) => "$attr = :$attr", $attributes));
+        $statement = self::prepare("DELETE FROM $tableName WHERE $sql");
+
+        foreach ($where as $key => $item) {
+            $statement->bindValue(":$key", $item);
+        }
+
+        $statement->execute();
+        return true;
+    }
 }
